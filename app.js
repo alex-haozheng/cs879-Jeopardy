918e9e87eaf3613d0cf277990010783a1d6ef08a
sazzle2 <azheng@umass.edu>
2024-04-13 13:58:21 -0400

    beginnign changes

---
+++ app.js
@@ -0,0 +1,9 @@
+const scoreDisplay = document.getElementById('score-display')
+const questionDisplay = document.getElementById('question-display')
+
+const questions = [
+	{
+		quiz: ['x', 'y', 'z'],
+		options: ['z']
+	}
+]
